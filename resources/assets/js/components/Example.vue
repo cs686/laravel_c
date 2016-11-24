e7ed4ad277e1d2fea29af92cceb9d9c0c8595252
cs686 <cs686@126.com>
2016-11-24 22:46:00 +0800

    first init

---
+++ resources/assets/js/components/Example.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10">
+                <div class="panel panel-default">
+                    <div class="panel-heading">控件测试</div>
+                    <div class="panel-body">
+                        我是一个控件
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component ready.')
+        }
+    }
+</script>
